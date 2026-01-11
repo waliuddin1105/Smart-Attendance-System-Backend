071b2572a0f3ce457bb8c7f61d5b0f68b3e47541
waliuddin1105 <waliahmed1105@gmail.com>
2026-01-11 12:18:50 +0500

    Added attendance fields

---
+++ student management system/api/fields/attendanceFields.py
@@ -0,0 +1,11 @@
+from sms import attendance_log_ns
+from flask_restx import fields
+
+attendance_log_data = attendance_log_ns.model(
+    "Attendance log data",
+    {
+        "employee_id" : fields.Integer(required = True, description = "Enter employee id"),
+        "timestamp" : fields.String(required = True, description = "The time the employee entered or left"),
+        "type" : fields.String(required = True, description = "Entry or exit")
+    }
+)
